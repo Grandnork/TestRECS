5ba68211ba7dbba1ab2e53a16981855ba9f6bd01
paulolopes <paulolopestr@yahoo.com>
2026-07-07 20:48:04 -0300

    Melhoria na validação de nomes

--- backend/Models/Person.cs
+++ backend/Models/Person.cs
@@ -10,8 +10,16 @@ public class Person
 */
     public Guid Id { get; private set; } = Guid.NewGuid();
 
+/*
+    Validação de nome refeita para realizar o bloqueio de numeros e grande parte dos simbolos, 
+    bloqueando a inserção de simbolos invalidos, mas levando em consideração nomes como "O'Connor".
+*/
     [Required]
     [MaxLength(100)]
+    [RegularExpression(
+        @"^[a-zA-ZÀ-ÿ\s'-]+$",
+        ErrorMessage = "Name contains invalid characters."
+    )]
     public string Name { get; set; } = string.Empty;
 
     [Range(0, 123)]
